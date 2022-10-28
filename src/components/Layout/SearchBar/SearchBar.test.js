fee201c8b50c4a2f7ae66d906c1650c6adb5eb0f
Viet Nguyen <105207060+VietNguyenR@users.noreply.github.com>
2022-10-28 23:14:25 +0700

    * Fix typo

--- src/components/Layout/SearchBar/SearchBar.test.js
+++ src/components/Layout/SearchBar/SearchBar.test.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import SearchBar from './SearchBar';
 
-test('Should search bar render', () => {
+test('Should search input render', () => {
   render(<SearchBar />);
   expect(screen.getByTestId('search-input')).toBeInTheDocument();
 });
