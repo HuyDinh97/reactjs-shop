24b7ae56fd222f75fa81dd993d3eccd3c3a7b196
Viet Nguyen <105207060+VietNguyenR@users.noreply.github.com>
2022-10-29 00:54:50 +0700

    * Add test case

--- src/components/Layout/SearchBar/SearchBar.test.js
+++ src/components/Layout/SearchBar/SearchBar.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import SearchBar from './SearchBar';
 
 test('Should search input render', () => {
   render(<SearchBar />);
   expect(screen.getByTestId('search-input')).toBeInTheDocument();
+});
+
+test('Should show cart dropdown', async () => {
+  render(<SearchBar />);
+
+  const buttons = await screen.findAllByRole('button');
+  expect(buttons).toHaveLength(2);
+
+  userEvent.click(buttons[1]);
+
+  const cartMenu = await screen.findByText('No items in this cart!');
+  expect(cartMenu).not.toBeNull();
 });
