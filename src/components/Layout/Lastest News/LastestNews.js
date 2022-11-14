ccc03452137287bd360ada373fc8ad54ae8cff6f
HuyDinh97 <hellboylk10@gmail.com>
2022-11-14 14:56:41 +0700

    lastestnews update

--- src/components/Layout/Lastest News/LastestNews.js
+++ src/components/Layout/Lastest News/LastestNews.js
@@ -11,25 +11,65 @@ function LastestNews() {
   return (
     <div className={classes.mg_4}>
       <TitleUnderline name="LASTEST NEWS" />
-      <Card className={classes.block_news}>
-        <Card.Img variant="top" src={cardImg1} />
-        <Card.Body>
-          <Card.Title className="fw-bolder h6">
-            SIMPLY TIPS FOR BEAUTY
-          </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Posted by
-            <span className={classes.comment_count}> admin - 2 Comments</span>
-          </Card.Subtitle>
-          <Card.Text className={classes.text_overflow}>
-            Bootstrap includes a few general use CSS transitions that can be
-            applied to a number of components.
-          </Card.Text>
-          <a className={classes.LastestNewsBtn} href="/">
-            READ MORE
-          </a>
-        </Card.Body>
-      </Card>
+      <div className="d-flex justify-content-between mt-3">
+        <Card className={classes.block_news}>
+          <Card.Img variant="top" src={cardImg1} />
+          <Card.Body>
+            <Card.Title className="fw-bolder h5 mb-3">
+              SIMPLY TIPS FOR BEAUTY
+            </Card.Title>
+            <Card.Subtitle className="mb-3 text-muted h5">
+              Posted by
+              <span className={classes.comment_count}> admin - 2 Comments</span>
+            </Card.Subtitle>
+            <Card.Text className={classes.text_overflow}>
+              Bootstrap includes a few general use CSS transitions that can be
+              applied to a number of components.
+            </Card.Text>
+            <a className={classes.LastestNewsBtn} href="/">
+              READ MORE
+            </a>
+          </Card.Body>
+        </Card>
+        <Card className={classes.block_news}>
+          <Card.Img variant="top" src={cardImg1} />
+          <Card.Body>
+            <Card.Title className="fw-bolder h5 mb-3">
+              SIMPLY TIPS FOR BEAUTY
+            </Card.Title>
+            <Card.Subtitle className="mb-3 text-muted h5">
+              Posted by
+              <span className={classes.comment_count}> admin - 2 Comments</span>
+            </Card.Subtitle>
+            <Card.Text className={classes.text_overflow}>
+              Bootstrap includes a few general use CSS transitions that can be
+              applied to a number of components.
+            </Card.Text>
+            <a className={classes.LastestNewsBtn} href="/">
+              READ MORE
+            </a>
+          </Card.Body>
+        </Card>
+        <Card className={classes.block_news}>
+          <Card.Img variant="top" src={cardImg1} />
+          <Card.Body>
+            <Card.Title className="fw-bolder h5 mb-3">
+              SIMPLY TIPS FOR BEAUTY
+            </Card.Title>
+            <Card.Subtitle className="mb-3 text-muted h5">
+              Posted by
+              <span className={classes.comment_count}> admin - 2 Comments</span>
+            </Card.Subtitle>
+            <Card.Text className={classes.text_overflow}>
+              Bootstrap includes a few general use CSS transitions that can be
+              applied to a number of components.
+            </Card.Text>
+            <a className={classes.LastestNewsBtn} href="/">
+              READ MORE
+            </a>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
