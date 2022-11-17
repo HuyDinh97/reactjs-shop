5478280e6d8c97c5e90021af14e2b46fb975f8f9
HuyDinh97 <hellboylk10@gmail.com>
2022-11-17 15:14:21 +0700

    About Us

---
+++ src/components/Layout/AboutUs/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './AboutUs.module.css';
+
+function AboutUs() {
+  return (
+    <div>
+      <div>1</div>
+    </div>
+  );
+}
+
+export default AboutUs;
