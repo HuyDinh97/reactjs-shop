0ec5c1d5adbf56284449731dc97c9068e8041b44
HuyDinh97 <hellboylk10@gmail.com>
2022-11-17 10:42:25 +0700

    SubscribeEmail update

--- src/components/Layout/Subscribe Email/SubscribeEmail.js
+++ src/components/Layout/Subscribe Email/SubscribeEmail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TitleUnderline from '../Popular Product/TitleUnderline';
+import { Col } from 'react-bootstrap';
 
 import classes from './SubscribeEmail.module.css';
 
@@ -7,11 +7,10 @@ function SubscribeEmail() {
   return (
     <div className={classes.subscribe_email}>
       <div className={classes.container}>
-        <TitleUnderline
-          name="FOLLOW YOUR UPDATE!"
-          className={classes.TitleUnderline}
-        />
-        <p className="text-white fs-4 d-flex justify-content-center my-4 col-10">
+        <Col className={classes.Product_title}>
+          <div className={classes.Underline}>FOLLOW YOUR UPDATE!</div>
+        </Col>
+        <p className="text-white fs-5 d-flex justify-content-center my-4 col-10 fw-semibold">
           If you want to get an email from us every time we have a new special
           offer, then sign up here!
         </p>
@@ -21,12 +20,7 @@ function SubscribeEmail() {
             className="form-control rounded-pill fw-semibold py-2 fs-6 d-flex justify-content-end"
             placeholder="Enter Your Email"
           />
-          <button
-            type="button"
-            className="rounded-pill py-2 px-5 fs-6 fw-semibold border-0 mx-4"
-          >
-            Subscribe
-          </button>
+          <button type="button">Subscribe</button>
         </div>
       </div>
     </div>
