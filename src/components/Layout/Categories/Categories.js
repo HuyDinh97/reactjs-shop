6b27c1be1cc6ba04804b8b4223d72cf724f7366b
HuyDinh97 <hellboylk10@gmail.com>
2022-12-05 11:24:43 +0700

    update fetch categories api

--- src/components/Layout/Categories/Categories.js
+++ src/components/Layout/Categories/Categories.js
@@ -1,4 +1,3 @@
-/* eslint-disable array-callback-return */
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
@@ -7,20 +6,17 @@ import classes from './Categories.module.css';
 
 function Categories() {
   const [categories, setCategories] = useState([]);
-  // eslint-disable-next-line no-undef
   useEffect(() => {
     axios
       .get('https://vnguyen.xyz/huy/day17/apis/index.php?type=categories')
       .then((res) => {
         console.log(res);
-        // eslint-disable-next-line no-unused-expressions
         setCategories(res.data.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
-  // eslint-disable-next-line array-callback-return
   return (
     <Nav defaultActiveKey="/home" as="ul">
       {categories.map((category) => (
