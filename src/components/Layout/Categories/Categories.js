0cb66f4bc0e3a63c49f5c61c1e44e7da854c1303
HuyDinh97 <hellboylk10@gmail.com>
2022-12-05 11:20:23 +0700

    fetch categories api

--- src/components/Layout/Categories/Categories.js
+++ src/components/Layout/Categories/Categories.js
@@ -1,26 +1,35 @@
-import React from 'react';
+/* eslint-disable array-callback-return */
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 
 import classes from './Categories.module.css';
 
 function Categories() {
+  const [categories, setCategories] = useState([]);
+  // eslint-disable-next-line no-undef
+  useEffect(() => {
+    axios
+      .get('https://vnguyen.xyz/huy/day17/apis/index.php?type=categories')
+      .then((res) => {
+        console.log(res);
+        // eslint-disable-next-line no-unused-expressions
+        setCategories(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  // eslint-disable-next-line array-callback-return
   return (
     <Nav defaultActiveKey="/home" as="ul">
-      <Nav.Item as="li">
-        <Nav.Link className={classes.nav_categories} href="/home">
-          Home
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link className={classes.nav_categories} eventKey="link-1">
-          Fashion Sneaker
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link className={classes.nav_categories} eventKey="link-2">
-          Jackets
-        </Nav.Link>
-      </Nav.Item>
+      {categories.map((category) => (
+        <Nav.Item key={category._id} as="li">
+          <Nav.Link className={classes.nav_categories} href="/">
+            {category.name}
+          </Nav.Link>
+        </Nav.Item>
+      ))}
     </Nav>
   );
 }
