31f36bf770fd5f155c6e8cae3ee1ce8d73e5d269
HuyDinh97 <hellboylk10@gmail.com>
2022-12-12 10:42:20 +0700

    Update CustomerComment.js

--- src/components/Layout/Customer comment/CustomerComment.js
+++ src/components/Layout/Customer comment/CustomerComment.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useRef } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import useFetchBlog from 'Hooks/useFetchBlog';
