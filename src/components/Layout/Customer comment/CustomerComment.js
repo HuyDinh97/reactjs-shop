c970b092e964d9b249cc9905376f43eb3b952456
HuyDinh97 <hellboylk10@gmail.com>
2022-12-20 10:27:39 +0700

    Update CustomerComment.js

--- src/components/Layout/Customer comment/CustomerComment.js
+++ src/components/Layout/Customer comment/CustomerComment.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
-<<<<<<< HEAD
-import useFetchBlog from 'Hooks/useFetchBlog';
-=======
 import useFetchTestimotional from 'Hooks/useFetchTestimotional';
->>>>>>> 6df556a645fdd993207090b36585764d536f464c
 import quoteImg from './image/icons8-quote-left-48.png';
 
 import 'swiper/swiper-bundle.min.css';
@@ -14,27 +10,33 @@ import 'swiper/swiper.min.css';
 import classes from './CustomerComment.module.css';
 
 function CustomerComment() {
-<<<<<<< HEAD
-  const { blogs, isSuccess } = useFetchBlog(); // Destructuring
+  const { testimotions, isSuccess } = useFetchTestimotional();
 
-  // Safe code: handle if API error in response
   if (isSuccess === false) {
-    return <p>Error, cannot fetch API</p>;
+    return <p data-testid="error-fetch-blog">Error, cannot fetch API</p>;
   }
 
-  if (blogs.length <= 0) {
+  if (!testimotions) {
     return <p>Loading...</p>;
   }
+
+  const linkIMG = 'https://vnguyen.xyz/huy/day17/apis/';
+
   return (
     <div>
-      <Swiper pagination modules={[Pagination]} className="mySwiper">
-        {blogs &&
-          blogs.map((blog) => (
-            <SwiperSlide className={classes.Box} key={blog._id}>
+      <Swiper
+        pagination
+        modules={[Pagination]}
+        className="mySwiper"
+        data-testid="testimotional-element"
+      >
+        {testimotions &&
+          testimotions.map((testi) => (
+            <SwiperSlide className={classes.Box} key={testi._id.$oid}>
               <div>
                 <div className={classes.Box_inside}>
-                  <div>
-                    <img src={blog.image} alt="img" />
+                  <div className={classes.Box_avatar}>
+                    <img src={linkIMG + testi.avatar} alt="img" />
                   </div>
                   <div>
                     <div>
@@ -46,74 +48,20 @@ function CustomerComment() {
                     </div>
                     <div>
                       <p className={classes.user_comment}>
-                        {blog.content.substring(0, 320)}
+                        {testi.feedback_text}
                       </p>
                     </div>
                     <div>
-                      <p className={classes.user_name}>{blog.author}</p>
+                      <p className={classes.user_name}>
+                        {' - '}
+                        {testi.customer_name}
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </SwiperSlide>
           ))}
-=======
-  const { testimotional, isSuccess } = useFetchTestimotional();
-
-  if (isSuccess === false) {
-    return <p data-testid="error-fetch-blog">Error, cannot fetch API</p>;
-  }
-
-  if (testimotional?.length <= 0) {
-    return <p>Loading...</p>;
-  }
-  const linkIMG = 'https://vnguyen.xyz/huy/day17/apis/';
-  return (
-    <div>
-      <Swiper
-        pagination
-        modules={[Pagination]}
-        className="mySwiper"
-        data-testid="testimotional-element"
-      >
-        {testimotional &&
-          testimotional.map((testi) => {
-            return (
-              <SwiperSlide
-                className={classes.Box}
-                key={testi._id.$oid.toString()}
-              >
-                <div>
-                  <div className={classes.Box_inside}>
-                    <div className={classes.Box_avatar}>
-                      <img src={linkIMG + testi.avatar} alt="img" />
-                    </div>
-                    <div>
-                      <div>
-                        <img
-                          className={classes.quote_w}
-                          src={quoteImg}
-                          alt="quoteImg"
-                        />
-                      </div>
-                      <div>
-                        <p className={classes.user_comment}>
-                          {testi.feedback_text}
-                        </p>
-                      </div>
-                      <div>
-                        <p className={classes.user_name}>
-                          {' - '}
-                          {testi.customer_name}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide>
-            );
-          })}
->>>>>>> 6df556a645fdd993207090b36585764d536f464c
       </Swiper>
     </div>
   );
