87f7a86f8e0c05fb92b7c4ec3d84ff5a0b53126c
Viet Nguyen <105207060+VietNguyenR@users.noreply.github.com>
2023-01-06 21:11:26 +0700

    * Massive refactor * Re-structure folder architect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import AppRouter from 'router/AppRouters';
+import configureStore from './store/configureStore';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'swiper/css/bundle';
 
+const store = configureStore();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
