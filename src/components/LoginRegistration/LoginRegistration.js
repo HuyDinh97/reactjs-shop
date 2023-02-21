abcc639a0384034069009d34d30e3e82b91cdd1e
HuyDinh97 <hellboylk10@gmail.com>
2023-02-21 16:31:40 +0700

    Update Login condition

--- src/components/LoginRegistration/LoginRegistration.js
+++ src/components/LoginRegistration/LoginRegistration.js
@@ -1,8 +1,79 @@
+/* eslint-disable no-alert */
+/* eslint-disable consistent-return */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useRef } from 'react';
 import classes from './LoginRegistration.module.css';
 
 function LoginRegistration() {
+  const postData = async (url = '', data = {}) => {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow',
+        body: new URLSearchParams(data),
+      });
+      return response.json();
+    } catch {
+      console.error('ss');
+    }
+  };
+
+  const emailLogIn = useRef();
+  const passwordLogIn = useRef();
+  const loginRemember = document.getElementById('loginRemember');
+
+  const setCookie = (email, value, minutes = 0) => {
+    let expires = '1';
+    if (minutes && minutes > 0) {
+      const date = new Date();
+      date.setTime(date.getTime() + minutes * 60 * 1000);
+      expires = date.toUTCString();
+    }
+    document.cookie = `${email}=${value}; expires=${expires}; path=/;`;
+  };
+
+  const doLogin = async () => {
+    const email = emailLogIn.current.value;
+    const password = passwordLogIn.current.value;
+    const remember = loginRemember.checked ? 5 : 1;
+
+    const regEmail =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regEmail.test(email)) {
+      alert(
+        'You must provide an correct email address to perform login action!'
+      );
+      return false;
+    }
+
+    if (!email) {
+      alert('You must provide an email address to perform login action!');
+      return false;
+    }
+    if (!password) {
+      alert('You must provide an password to perform login action!');
+      return false;
+    }
+
+    const response = await postData(
+      'https://vnguyen.xyz/huy/day15/apis/signin.php',
+      {
+        email,
+        password,
+      }
+    );
+
+    if (response?.status) {
+      setCookie('email', email, remember);
+      window.location.href = '/';
+      return false;
+    }
+    alert(response.message);
+  };
+
   return (
     <div className={classes.LoginRegistration}>
       <ul>
@@ -19,20 +90,24 @@ function LoginRegistration() {
           <input
             className={classes.inputText}
             placeholder="Enter Your Email"
-            id="email"
+            id="emailLogin"
+            type="email"
+            ref={emailLogIn}
           />
         </li>
         <li>
           <input
             className={classes.inputText}
             placeholder="Enter Your Password"
-            id="password"
+            id="passwordLogin"
+            type="password"
+            ref={passwordLogIn}
           />
         </li>
         <li className="d-flex justify-content-between">
-          <span>
-            <input type="checkbox" id="scales" name="scales" />
-            <label className="mx-1" htmlFor="scales">
+          <span className="d-flex">
+            <input type="checkbox" id="loginRemember" name="loginRemember" />
+            <label className="mx-1" htmlFor="loginRemember">
               Remember Me
             </label>
           </span>
@@ -41,7 +116,11 @@ function LoginRegistration() {
           </span>
         </li>
         <li>
-          <button className={classes.login_button} type="button">
+          <button
+            className={classes.login_button}
+            type="button"
+            onClick={doLogin}
+          >
             Login
           </button>
         </li>
@@ -67,18 +146,18 @@ function LoginRegistration() {
           <input
             className={classes.inputText}
             placeholder="Enter Your Email"
-            id="email"
+            id="emailSignUp"
           />
         </li>
         <li>
           <input
             className={classes.inputText}
             placeholder="Enter Your Password"
-            id="password"
+            id="passwordSignUp"
           />
         </li>
         <li className="d-flex justify-content-between">
-          <span>
+          <span className="d-flex">
             <input type="checkbox" id="scales" name="scales" />
             <label className="mx-1" htmlFor="scales">
               Accept the terms and conditions
