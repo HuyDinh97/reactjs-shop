b91b98808123b126a3f605b2754a007beed34e2e
HuyDinh97 <hellboylk10@gmail.com>
2023-03-07 14:32:04 +0700

    cartDetail demo

--- src/components/CartTable/CartTable.js
+++ src/components/CartTable/CartTable.js
@@ -2,76 +2,79 @@
 import CartTotal from 'components/CartTotal/CartTotal';
 import PromotionCode from 'components/PromotionCode/PromotionCode';
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { FaMinus, FaPlus, FaChevronLeft } from 'react-icons/fa';
 import { HiRefresh } from 'react-icons/hi';
 
+import { Link } from 'react-router-dom';
 import classes from './CartTable.module.css';
 import image from './image/product-img-3.jpg';
 
 function CartTable() {
   return (
     <div className={classes.mt_5}>
-      <Table className={classes.cartTable} striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Product Name</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <div className="d-flex justify-content-center" colSpan={2}>
-                <img src={image} alt="" />
+      <Container>
+        <Row className={classes.cartDetailHeader}>
+          <Col xs={2}>Item</Col>
+          <Col xs={5}>Product Name</Col>
+          <Col xs={1}>Price</Col>
+          <Col xs={2}>Quantity</Col>
+          <Col xs={1}>Subtotal</Col>
+          <Col xs={1}> </Col>
+        </Row>
+        <Row className={classes.cartDetail}>
+          <Col xs={2}>
+            <div className="d-flex justify-content-center border-0">
+              <img src={image} alt="" />
+            </div>
+          </Col>
+          <Col className={classes.productName} xs={5}>
+            Calvin Klein womens Solid Sheath With Chiffon Bell Sleeves Dress
+          </Col>
+          <Col className={classes.price_sample} xs={1}>
+            $6
+          </Col>
+          <Col xs={2}>
+            <div className="d-flex justify-content-center align-items-center border-0">
+              <button className={classes.quantity_button}>
+                <FaMinus />
+              </button>
+              <div className={classes.quantity_in_cart}>
+                <input type="tel" min={0} aria-label="couple-code" />
               </div>
-            </td>
-            <td className="fs-5 fw-semibold">
-              Calvin Klein womens Solid Sheath With Chiffon Bell Sleeves Dress
-            </td>
-            <td className={classes.price_sample}>$6</td>
-            <td>
-              <div className="d-flex justify-content-center align-items-center">
-                <button className={classes.quantity_button}>
-                  <FaMinus />
+              <button className={classes.quantity_button}>
+                <FaPlus />
+              </button>
+            </div>
+          </Col>
+          <Col className={classes.price_sample} xs={1}>
+            $6
+          </Col>
+          <Col xs={1}>
+            <button className={classes.deleteButton}>X</button>
+          </Col>
+        </Row>
+        <Row className={classes.cartDetail}>
+          <Col>
+            <div className="d-flex justify-content-center align-item-center border-0 p-0">
+              <span className={classes.buttonUnderCartBorder}>
+                <Link to="/" className={classes.buttonUnderCart}>
+                  <FaChevronLeft className={classes.margin_right_1rem} />
+                  CONTINUE SHOPPING
+                </Link>
+              </span>
+              <span className={classes.buttonUpdateCartBorder}>
+                <button className={classes.buttonUpdateCart}>
+                  <HiRefresh className={classes.margin_right_1rem} />
+                  UPDATE CART
                 </button>
-                <div className={classes.quantity_in_cart}>
-                  <input type="tel" min={0} aria-label="couple-code" />
-                </div>
-                <button className={classes.quantity_button}>
-                  <FaPlus />
-                </button>
-              </div>
-            </td>
-            <td className={classes.price_sample}>$6</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan={5}>
-              <div className="d-flex justify-content-center align-item-center">
-                <span>
-                  <button href="/">
-                    <a href="/">
-                      <FaChevronLeft className={classes.margin_right_1rem} />
-                      CONTINUE SHOPPING
-                    </a>
-                  </button>
-                </span>
-                <span>
-                  <button className={classes.update_cart}>
-                    <HiRefresh className={classes.margin_right_1rem} />
-                    UPDATE CART
-                  </button>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tfoot>
-      </Table>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
       <div className="d-flex justify-content-between">
         <PromotionCode />
         <CartTotal />
