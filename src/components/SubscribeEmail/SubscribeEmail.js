220fca54592f5099f8f0355c71329ab5d6cc2d43
HuyDinh97 <hellboylk10@gmail.com>
2023-03-13 13:14:45 +0700

    update subcribeEmail responsive

--- src/components/SubscribeEmail/SubscribeEmail.js
+++ src/components/SubscribeEmail/SubscribeEmail.js
@@ -7,14 +7,14 @@ function SubscribeEmail() {
   return (
     <div className={classes.subscribe_email}>
       <div className={classes.container}>
-        <Col className={classes.Product_title}>
+        <Col col className={classes.Product_title}>
           <div className={classes.Underline}>FOLLOW YOUR UPDATE!</div>
         </Col>
-        <p className="text-white fs-5 d-flex justify-content-center my-4 col-10 fw-semibold">
+        <p className={classes.emailDescribe}>
           If you want to get an email from us every time we have a new special
           offer, then sign up here!
         </p>
-        <div className="d-flex justify-content-center my-4 col-6">
+        <div className={classes.emailSubcribeSubmit}>
           <input
             type="email"
             className="form-control rounded-pill fw-semibold py-2 fs-6 d-flex justify-content-end"
