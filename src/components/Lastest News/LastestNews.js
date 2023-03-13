3c2b9dcd1c1867a712d9519e8838246cf5d35852
HuyDinh97 <hellboylk10@gmail.com>
2023-03-13 11:39:08 +0700

    update lastestNews responsive

--- src/components/Lastest News/LastestNews.js
+++ src/components/Lastest News/LastestNews.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import TitleUnderline from '../PopularProduct/TitleUnderline';
 import cardImg1 from './image/news-img-1.jpg';
 import cardImg2 from './image/news-img-2.jpg';
@@ -11,64 +13,81 @@ function LastestNews() {
   return (
     <div className={classes.mg_4}>
       <TitleUnderline name="LASTEST NEWS" />
-      <div className="d-flex justify-content-between mt-3">
-        <Card className={classes.block_news}>
-          <Card.Img variant="top" src={cardImg1} />
-          <Card.Body>
-            <Card.Title className="fw-bolder h5 mb-3">
-              SIMPLY TIPS FOR BEAUTY
-            </Card.Title>
-            <Card.Subtitle className="mb-3 text-muted h5">
-              Posted by
-              <span className={classes.comment_count}> admin - 2 Comments</span>
-            </Card.Subtitle>
-            <Card.Text className={classes.text_overflow}>
-              Bootstrap includes a few general use CSS transitions that can be
-              applied to a number of components.
-            </Card.Text>
-            <a className={classes.LastestNewsBtn} href="/">
-              READ MORE
-            </a>
-          </Card.Body>
-        </Card>
-        <Card className={classes.block_news}>
-          <Card.Img variant="top" src={cardImg2} />
-          <Card.Body>
-            <Card.Title className="fw-bolder h5 mb-3">
-              SIMPLY TIPS FOR BEAUTY
-            </Card.Title>
-            <Card.Subtitle className="mb-3 text-muted h5">
-              Posted by
-              <span className={classes.comment_count}> admin - 2 Comments</span>
-            </Card.Subtitle>
-            <Card.Text className={classes.text_overflow}>
-              Bootstrap includes a few general use CSS transitions that can be
-              applied to a number of components.
-            </Card.Text>
-            <a className={classes.LastestNewsBtn} href="/">
-              READ MORE
-            </a>
-          </Card.Body>
-        </Card>
-        <Card className={classes.block_news}>
-          <Card.Img variant="top" src={cardImg3} />
-          <Card.Body>
-            <Card.Title className="fw-bolder h5 mb-3">
-              SIMPLY TIPS FOR BEAUTY
-            </Card.Title>
-            <Card.Subtitle className="mb-3 text-muted h5">
-              Posted by
-              <span className={classes.comment_count}> admin - 2 Comments</span>
-            </Card.Subtitle>
-            <Card.Text className={classes.text_overflow}>
-              Bootstrap includes a few general use CSS transitions that can be
-              applied to a number of components.
-            </Card.Text>
-            <a className={classes.LastestNewsBtn} href="/">
-              READ MORE
-            </a>
-          </Card.Body>
-        </Card>
+      <div className={classes.lastestNews}>
+        <Row>
+          <Col className="d-flex justify-content-center py-2">
+            <Card className={classes.block_news}>
+              <Card.Img variant="top" src={cardImg1} />
+              <Card.Body>
+                <Card.Title className={classes.cardTitle}>
+                  SIMPLY TIPS FOR BEAUTY
+                </Card.Title>
+                <Card.Subtitle className={classes.subtitle}>
+                  Posted by
+                  <span className={classes.comment_count}>
+                    {' '}
+                    admin - 2 Comments
+                  </span>
+                </Card.Subtitle>
+                <Card.Text className={classes.text_overflow}>
+                  Bootstrap includes a few general use CSS transitions that can
+                  be applied to a number of components.
+                </Card.Text>
+                <a className={classes.LastestNewsBtn} href="/">
+                  READ MORE
+                </a>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex justify-content-center py-2">
+            <Card className={classes.block_news}>
+              <Card.Img variant="top" src={cardImg2} />
+              <Card.Body>
+                <Card.Title className={classes.cardTitle}>
+                  SIMPLY TIPS FOR BEAUTY
+                </Card.Title>
+                <Card.Subtitle className={classes.subtitle}>
+                  Posted by
+                  <span className={classes.comment_count}>
+                    {' '}
+                    admin - 2 Comments
+                  </span>
+                </Card.Subtitle>
+                <Card.Text className={classes.text_overflow}>
+                  Bootstrap includes a few general use CSS transitions that can
+                  be applied to a number of components.
+                </Card.Text>
+                <a className={classes.LastestNewsBtn} href="/">
+                  READ MORE
+                </a>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex justify-content-center py-2">
+            <Card className={classes.block_news}>
+              <Card.Img variant="top" src={cardImg3} />
+              <Card.Body>
+                <Card.Title className={classes.cardTitle}>
+                  SIMPLY TIPS FOR BEAUTY
+                </Card.Title>
+                <Card.Subtitle className={classes.subtitle}>
+                  Posted by
+                  <span className={classes.comment_count}>
+                    {' '}
+                    admin - 2 Comments
+                  </span>
+                </Card.Subtitle>
+                <Card.Text className={classes.text_overflow}>
+                  Bootstrap includes a few general use CSS transitions that can
+                  be applied to a number of components.
+                </Card.Text>
+                <a className={classes.LastestNewsBtn} href="/">
+                  READ MORE
+                </a>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
