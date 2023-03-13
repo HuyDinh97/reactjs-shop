d1d857fb210ccf9e04b5ddd502de2f668255ea29
HuyDinh97 <hellboylk10@gmail.com>
2023-03-13 15:02:58 +0700

    update serviceBanner responsive

--- src/components/ServiceBanner/ServiceBanner.js
+++ src/components/ServiceBanner/ServiceBanner.js
@@ -10,65 +10,65 @@ import classes from './ServiceBanner.module.css';
 
 function ServiceBanner() {
   return (
-    <div>
-      <Container className={classes.ServiceBanner_box}>
+    <div className={classes.serviceBanner}>
+      <Container className={classes.ServiceBannerBox}>
         <Row>
-          <Col>
-            <div className={classes.ServiceBanner_box}>
-              <div className="border-end d-flex align-items-center flex-row px-5">
+          <Col xl={3} xs={6} className={classes.blockBoderEnd}>
+            <div className={classes.ServiceBannerBox}>
+              <div className={classes.ServiceBannerBlock}>
                 <div className="d-flex mx-3 flip-img fs-1">
-                  <span className={classes.ServiceBanner_truck_icon}>
+                  <span className={classes.ServiceBannerIcon}>
                     <ImTruck />
                   </span>
                 </div>
                 <div className={classes.w_max_content}>
                   <h5 className="fw-bold mt-2">FREE DELIVERY</h5>
-                  <div className={classes.icon_subtitle}>WORLDWIDE</div>
+                  <div className={classes.iconSubtitle}>WORLDWIDE</div>
                 </div>
               </div>
             </div>
           </Col>
-          <Col>
-            <div className={classes.ServiceBanner_box}>
-              <div className="border-end d-flex align-items-center flex-row px-5">
+          <Col xl={3} xs={6}>
+            <div className={classes.ServiceBannerBox}>
+              <div className={classes.ServiceBannerBlock}>
                 <div className="d-flex mx-3 flip-img fs-1">
-                  <span className={classes.ServiceBanner_truck_icon}>
+                  <span className={classes.ServiceBannerIcon}>
                     <FaHeadphones />
                   </span>
                 </div>
                 <div className={classes.w_max_content}>
                   <h5 className="fw-bold mt-2">24/7 SUPPORT</h5>
-                  <div className={classes.icon_subtitle}>CUSTOMER SUPPORT</div>
+                  <div className={classes.iconSubtitle}>CUSTOMER SUPPORT</div>
                 </div>
               </div>
             </div>
           </Col>
-          <Col>
-            <div className={classes.ServiceBanner_box}>
-              <div className="border-end d-flex align-items-center flex-row px-5">
+          <Col xl={3} xs={6} className={classes.underBlockLeft}>
+            <div className={classes.ServiceBannerBox}>
+              <div className={classes.ServiceBannerBlock}>
                 <div className="d-flex mx-3 flip-img fs-1">
-                  <span className={classes.ServiceBanner_truck_icon}>
-                    <ImTrophy />
-                  </span>
-                </div>
-                <div className={classes.w_max_content}>
-                  <h5 className="fw-bold mt-2">PAYMENT</h5>
-                  <div className={classes.icon_subtitle}>SECURE SYSTEM</div>
-                </div>
-              </div>
-            </div>
-          </Col>
-          <Col>
-            <div className={classes.ServiceBanner_box}>
-              <div className="border-end d-flex align-items-center flex-row px-5">
-                <div className="d-flex mx-3 flip-img fs-1">
-                  <span className={classes.ServiceBanner_truck_icon}>
+                  <span className={classes.ServiceBannerIcon}>
                     <FaCcMastercard />
                   </span>
                 </div>
                 <div className={classes.w_max_content}>
+                  <h5 className="fw-bold mt-2">PAYMENT</h5>
+                  <div className={classes.iconSubtitle}>SECURE SYSTEM</div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col xl={3} xs={6} className={classes.underBlockRight}>
+            <div className={classes.ServiceBannerBox}>
+              <div className={classes.ServiceBannerBlock}>
+                <div className="d-flex mx-3 flip-img fs-1">
+                  <span className={classes.ServiceBannerIcon}>
+                    <ImTrophy />
+                  </span>
+                </div>
+                <div className={classes.w_max_content}>
                   <h5 className="fw-bold mt-2">TRUSTED</h5>
-                  <div className={classes.icon_subtitle}>ENUINE PRODUCTS</div>
+                  <div className={classes.iconSubtitle}>ENUINE PRODUCTS</div>
                 </div>
               </div>
             </div>
