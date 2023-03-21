edee6a39db132e696831ad8b314e33e1d24e4ede
HuyDinh97 <hellboylk10@gmail.com>
2023-03-21 09:42:22 +0700

    update SummerSale responsive

--- src/components/SummerSale/SummerSale.js
+++ src/components/SummerSale/SummerSale.js
@@ -1,47 +1,42 @@
+/* eslint-disable react/forbid-dom-props */
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
-import slide1 from './sales/slider-img-1.jpg';
-import slide2 from './sales/slider-img-2.jpg';
-import slide3 from './sales/slider-img-3.jpg';
+import { Link } from 'react-router-dom';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 
+import { useGetHome } from 'store/selectors/common';
 import classes from './SummerSale.module.css';
 
 function SummerSale() {
+  const homeData = useGetHome();
+  if (!homeData) {
+    return <p>...</p>;
+  }
+  const { slider: sliders } = homeData ?? {};
+  console.log(`sliders`, sliders);
   return (
     <Swiper pagination modules={[Pagination]} className={classes.swiper_height}>
-      <SwiperSlide>
-        <img src={slide1} alt="slide" />
-        <div className={classes.slide_1}>
-          <h1>SUMMER SALE</h1>
-          <p>Get up to 50% off your shopping items tomorrow!</p>
-          <button type="button" className={classes.button}>
-            Shop Now
-          </button>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="slide" />
-        <div className={classes.slide_2}>
-          <h1>SUMMER SALE</h1>
-          <p>Get up to 50% off your shopping items tomorrow!</p>
-          <button type="button" className={classes.button}>
-            Shop Now
-          </button>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="slide" />
-        <div className={classes.slide_3}>
-          <h1>SUMMER SALE</h1>
-          <p>Get up to 50% off your shopping items tomorrow!</p>
-          <button type="button" className={classes.button}>
-            Shop Now
-          </button>
-        </div>
-      </SwiperSlide>
+      {sliders &&
+        sliders.map((banner) => (
+          <SwiperSlide
+            key={banner._id}
+            style={{
+              background: `url('https://vnguyen.xyz/huy/day17/apis/${banner.image}') center center / cover no-repeat`,
+            }}
+          >
+            <div className={`${classes.slideBanner} container`}>
+              <h1 className="text-uppercase">{banner.headline}</h1>
+              <p>{banner.sort}</p>
+              <Link to={`/${banner.interact}`}>
+                <button type="button" className={classes.button}>
+                  Shop Now
+                </button>
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
     </Swiper>
   );
 }
