6b195c3f147102f06b5ed9c6427076e4d71008a4
HuyDinh97 <hellboylk10@gmail.com>
2023-03-21 09:45:24 +0700

    Update SummerSale.js

--- src/components/SummerSale/SummerSale.js
+++ src/components/SummerSale/SummerSale.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/forbid-dom-props */
+/* eslint-disable react/forbid-component-props */
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
