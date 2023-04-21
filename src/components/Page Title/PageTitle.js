c02647558de6e2e00c2ab31505e0234f1bb9a258
HuyDinh97 <hellboylk10@gmail.com>
2023-04-21 12:51:10 +0700

    Update PageTitle.js

--- src/components/Page Title/PageTitle.js
+++ src/components/Page Title/PageTitle.js
@@ -1,11 +1,14 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import classes from './PageTitle.module.css';
 
-function PageTitle() {
+function PageTitle({ pageTitle, pageLink }) {
   return (
-    <div className={classes.pageTitle}>
-      <h1 className={classes.pageTitleColor}>MY CART</h1>
-      <p>Home / Shop / Cart</p>
+    <div className={`${classes.pageTitle} py-5`}>
+      <h1 className={`${classes.pageTitleColor} text-uppercase`}>
+        {pageTitle}
+      </h1>
+      <p className="fw-semibold">Home / Shop / {pageLink}</p>
     </div>
   );
 }
