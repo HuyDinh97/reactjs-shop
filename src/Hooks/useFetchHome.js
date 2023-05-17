975eb5b2d8ee1b3b636dd3bd531b19172c771b53
HuyDinh97 <hellboylk10@gmail.com>
2023-05-17 16:24:36 +0700

    Update useFetchHome.js

--- src/Hooks/useFetchHome.js
+++ src/Hooks/useFetchHome.js
@@ -5,26 +5,20 @@ import { addHome } from 'store/actions/common';
 import { useGetHome } from 'store/selectors/common';
 
 const useFetchHome = () => {
-  const [isSuccess, setIssuccess] = React.useState();
   const dispatch = useDispatch();
   const homeData = useGetHome();
 
   React.useEffect(() => {
     if (homeData) {
-      setIssuccess(true);
       return;
     }
-
-    axios
-      .get('https://vnguyen.xyz/huy/day17/apis/index.php?type=home')
-      .then((res) => {
-        dispatch(addHome(res?.data ?? {}));
-        setIssuccess(true);
-      })
-      .catch(() => setIssuccess(false));
+    fetch('https://vnguyen.xyz/huy/day17/apis/index.php?type=home')
+      .then((res) => res.json())
+      .then((home) => dispatch(addHome(home ?? {})))
+      .catch(() => console.log());
   }, [homeData, dispatch]);
 
-  return { homeData, isSuccess };
+  return { homeData };
 };
 
 export default useFetchHome;
