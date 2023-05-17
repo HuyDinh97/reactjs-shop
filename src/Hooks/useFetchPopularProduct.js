e1c8d9d4b1de3b1c1f69fa935814d7138cea26d9
HuyDinh97 <hellboylk10@gmail.com>
2023-05-17 16:24:37 +0700

    Update useFetchPopularProduct.js

--- src/Hooks/useFetchPopularProduct.js
+++ src/Hooks/useFetchPopularProduct.js
@@ -1,29 +1,25 @@
-import axios from 'axios';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useGetPopularProduct } from 'store/selectors/common';
 import { addPopularProduct } from 'store/actions/common';
 
 const useFetchPopularProduct = () => {
-  const [isSuccess, setIssuccess] = React.useState();
   const dispatch = useDispatch();
   const popularProducts = useGetPopularProduct();
 
   React.useEffect(() => {
     if (popularProducts?.length > 0) {
-      setIssuccess(true);
       return;
     }
-    axios
-      .get('https://vnguyen.xyz/huy/day17/apis/index.php?type=products')
-      .then((res) => {
-        dispatch(addPopularProduct({ popularProducts: res.data.data }));
-        setIssuccess(true);
-      })
-      .catch(() => setIssuccess(false));
+    fetch('https://vnguyen.xyz/huy/day17/apis/index.php?type=products')
+      .then((res) => res.json())
+      .then((product) =>
+        dispatch(addPopularProduct({ popularProducts: product.data }))
+      )
+      .catch(() => console.log());
   }, [popularProducts?.length, dispatch]);
 
-  return { popularProducts, isSuccess };
+  return { popularProducts };
 };
 
 export default useFetchPopularProduct;
