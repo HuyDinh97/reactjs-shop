35b01bd50c6e0da177f1fd905a14bec6305f618d
HuyDinh97 <hellboylk10@gmail.com>
2023-05-18 11:17:02 +0700

    Update useFetchCategory.js

--- src/Hooks/useFetchCategory.js
+++ src/Hooks/useFetchCategory.js
@@ -4,13 +4,11 @@ import { useGetCategories } from 'store/selectors/common';
 import { addCategories } from '../store/actions/common';
 
 const useFetchCategory = () => {
-  const [isSuccess, setIssuccess] = React.useState();
   const dispatch = useDispatch();
   const categories = useGetCategories();
 
   React.useEffect(() => {
     if (categories?.length > 0) {
-      setIssuccess(true);
       return;
     }
     fetch('https://vnguyen.xyz/huy/day17/apis/index.php?type=categories')
@@ -18,10 +16,10 @@ const useFetchCategory = () => {
       .then((category) =>
         dispatch(addCategories({ categories: category.data }))
       )
-      .catch(() => setIssuccess(false));
+      .catch(() => console.log());
   }, [categories?.length, dispatch]);
 
-  return { categories, isSuccess };
+  return categories;
 };
 
 export default useFetchCategory;
