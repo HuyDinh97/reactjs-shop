258cb7fcbf7ad482b4d56ed7cabd01fd659d0f8f
HuyDinh97 <hellboylk10@gmail.com>
2023-05-18 11:17:00 +0700

    Update useFetchBlog.js

--- src/Hooks/useFetchBlog.js
+++ src/Hooks/useFetchBlog.js
@@ -1,18 +1,16 @@
-import axios from 'axios';
 import React from 'react';
 
 const useFetchBlog = () => {
   const [blogs, setBlogs] = React.useState();
-  const [isSuccess, setIssuccess] = React.useState();
 
   React.useEffect(() => {
     fetch('https://vnguyen.xyz/huy/day17/apis/index.php?type=posts')
       .then((res) => res.json())
       .then((data) => setBlogs(data.data))
-      .catch(() => setIssuccess(false));
+      .catch(() => console.log());
   }, []);
 
-  return { blogs, isSuccess };
+  return blogs;
 };
 
 export default useFetchBlog;
