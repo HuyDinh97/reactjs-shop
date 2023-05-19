a9ba45032faaaf524d59cb258819900d0d706345
HuyDinh97 <hellboylk10@gmail.com>
2023-05-19 17:36:30 +0700

    Update useFetchProductDetail.js

--- src/Hooks/useFetchProductDetail.js
+++ src/Hooks/useFetchProductDetail.js
@@ -2,18 +2,10 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { productDetail } from 'store/actions/common';
 
-import { useGetProductDetail } from 'store/selectors/common';
-
 const useFetchProductDetail = (id) => {
-  const comments = useGetProductDetail();
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    const commentExists = comments.find(
-      // eslint-disable-next-line camelcase
-      ({ product_id }) => product_id?.toString() === id
-    );
-    if (commentExists) return;
     fetch(`https://vnguyen.xyz/huy/day17/apis/index.php?type=product&id=${id}`)
       .then((res) => res.json())
       .then((productDetailData) =>
