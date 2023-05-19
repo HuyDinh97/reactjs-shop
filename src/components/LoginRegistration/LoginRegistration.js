566619cc665ec5a39c0fafc5cf184bf95fe72320
HuyDinh97 <hellboylk10@gmail.com>
2023-05-19 13:50:18 +0700

    Update LoginRegistration.js

--- src/components/LoginRegistration/LoginRegistration.js
+++ src/components/LoginRegistration/LoginRegistration.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-alert */
 /* eslint-disable consistent-return */
 /* eslint-disable jsx-a11y/label-has-associated-control */
