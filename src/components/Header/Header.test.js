3cb69517828bebbf4660a8865b83a520287d17a8
HuyDinh97 <hellboylk10@gmail.com>
2023-05-19 16:44:42 +0700

    Update Header.test.js

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+import { MemoryRouter } from 'react-router-dom';
 import Header from './Header';
 
 test('should Header render correctly', () => {
-  render(<Header />);
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
   expect(screen).toMatchSnapshot();
 });
