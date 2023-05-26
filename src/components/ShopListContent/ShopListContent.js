6062c19ed4dcb6f82782e8b9f13d63aed2dd7328
HuyDinh97 <hellboylk10@gmail.com>
2023-05-26 13:49:36 +0700

    update filter price color

--- src/components/ShopListContent/ShopListContent.js
+++ src/components/ShopListContent/ShopListContent.js
@@ -9,6 +9,22 @@ function ShopListContent() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const CustomSliderStyles = {
+    '& .MuiSlider-thumb': {
+      color: 'white',
+      border: '3px solid black',
+    },
+    '& .MuiSlider-track': {
+      color: 'black',
+    },
+    '& .MuiSlider-root ': {
+      width: '94%!important',
+    },
+    '& .MuiSlider-rail  ': {
+      color: 'black',
+    },
+  };
+
   return (
     <div className="my-5">
       <Container>
@@ -46,15 +62,17 @@ function ShopListContent() {
                 <li className="py-3">
                   <span className="fw-bold fs-5">FILTERS </span>
                 </li>
-                <li>
+                <li className="border-0 pt-4 pb-2 d-flex justify-content-center">
                   <Slider
+                    sx={CustomSliderStyles}
+                    className={classes.filterSlider}
                     value={value}
                     onChange={handleChange}
                     valueLabelDisplay="auto"
                     max={1000}
                   />
                 </li>
-                <li>
+                <li className="border-0">
                   <div className="fw-semibold fs-5">
                     <span className={classes.filterPrice}>PRICE </span>
                     <span className="text-black">
