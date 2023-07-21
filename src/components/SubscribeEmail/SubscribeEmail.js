6fb69d4d51c254ff58ac9e9414a5136eb35580d0
HuyDinh97 <hellboylk10@gmail.com>
2023-07-21 10:15:44 +0700

    Update SubscribeEmail.js

--- src/components/SubscribeEmail/SubscribeEmail.js
+++ src/components/SubscribeEmail/SubscribeEmail.js
@@ -43,41 +43,43 @@ function SubscribeEmail() {
   const handleClose = () => setShow(false);
 
   return (
-    <div
-      className={`${classes.subscribe_email} ${
-        subcribeEmailCheck === 'true' ? 'd-none' : ''
-      }`}
-    >
-      <Row className={classes.container}>
-        <Col className={classes.Product_title}>
-          <div className={classes.Underline}>FOLLOW YOUR UPDATE!</div>
-        </Col>
-        <Col className={classes.emailDescribe}>
-          If you want to get an email from us every time we have a new special
-          offer, then sign up here!
-        </Col>
-        <Col className={classes.emailSubcribeSubmit}>
-          <input
-            type="email"
-            ref={emailSubcribe}
-            className="form-control rounded-pill fw-semibold py-2 fs-6 d-flex justify-content-end"
-            placeholder="Enter Your Email"
-            data-testid="email-input"
-          />
-          <Button onClick={subcribe}>Subscribe</Button>
-          <Modal show={show} onHide={handleClose} centered size="md">
-            <Modal.Body className="d-flex justify-content-center fw-semibold py-4">
-              {subscribeErrors}
-            </Modal.Body>
-            <Button
-              className={`${classes.modelButton} border-0 py-2`}
-              onClick={handleClose}
-            >
-              Close
-            </Button>
-          </Modal>
-        </Col>
-      </Row>
+    <div>
+      {!subcribeEmailCheck ? (
+        <div className={classes.subscribe_email}>
+          <Row className={classes.container}>
+            <Col className={classes.Product_title}>
+              <div className={classes.Underline}>FOLLOW YOUR UPDATE!</div>
+            </Col>
+            <Col className={classes.emailDescribe}>
+              If you want to get an email from us every time we have a new
+              special offer, then sign up here!
+            </Col>
+            <Col className={classes.emailSubcribeSubmit}>
+              <input
+                type="email"
+                ref={emailSubcribe}
+                className="form-control rounded-pill fw-semibold py-2 fs-6 d-flex justify-content-end"
+                placeholder="Enter Your Email"
+                data-testid="email-input"
+              />
+              <Button onClick={subcribe}>Subscribe</Button>
+            </Col>
+          </Row>
+        </div>
+      ) : (
+        ''
+      )}
+      <Modal show={show} onHide={handleClose} centered size="md">
+        <Modal.Body className="d-flex justify-content-center fw-semibold py-4">
+          {subscribeErrors}
+        </Modal.Body>
+        <Button
+          className={`${classes.modelButton} border-0 py-2`}
+          onClick={handleClose}
+        >
+          Close
+        </Button>
+      </Modal>
     </div>
   );
 }
