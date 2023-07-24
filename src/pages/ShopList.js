a5f2afd97e4567e0bcd1e6f3e6ddbe9303121247
HuyDinh97 <hellboylk10@gmail.com>
2023-07-24 15:30:52 +0700

    Update ShopList.js

--- src/pages/ShopList.js
+++ src/pages/ShopList.js
@@ -3,23 +3,34 @@ import SearchBar from 'components/SearchBar/SearchBar';
 import ShopListContent from 'components/ShopListContent/ShopListContent';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { categoryChange, colorChange } from 'shared/ulti';
 import { useGetPopularProduct } from 'store/selectors/common';
 
 function ShopList() {
   const { id: idCategory } = useParams();
   const popularProducts = useGetPopularProduct();
+  const [categoryName] = categoryChange.filter((cate) =>
+    cate.name === idCategory ? cate.id : ''
+  );
+  const [colorName] = colorChange.filter((color) =>
+    color.name === idCategory ? color.id : ''
+  );
   const productFilterDefault =
     idCategory === 'all'
       ? popularProducts
       : popularProducts?.filter((prod) =>
-          prod.category.toString() === idCategory
-            ? prod.category.toString() === idCategory
-            : prod.color.toString() === idCategory
+          prod.category === categoryName?.id
+            ? prod.category === categoryName?.id
+            : prod.color === colorName?.id
         );
+
   return (
     <div>
       <SearchBar />
-      <PageTitle pageTitle="Shop list" pageLink="Shop List" />
+      <PageTitle
+        pageTitle={idCategory === 'all' ? 'Shop List' : idCategory}
+        pageLink={idCategory === 'all' ? 'Shop List' : idCategory}
+      />
       <ShopListContent productTo={productFilterDefault} />
     </div>
   );
